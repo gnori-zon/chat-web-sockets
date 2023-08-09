fdd027b6a4d3ee753e3afa6683d60b1856ecb82a
gnori-zon <konasenkos@gmail.com>
2023-08-09 23:23:33 +0300

    #1 #add #global - added user entity, local config-file, migrations
    added AbstractEntity, User, Role, migrations (create user table, user_role table), local config-file(added in for connect PostgreSql and activated liquibase), empty prod config-file

---
+++ chat-web-sockets/src/main/resources/db/changelog/changesets/V1691609165_create_table_users.sql
@@ -0,0 +1,17 @@
+-- liquibase formatted sql
+
+-- changeset gnori-zon:V1691609165_create_table_users:\nGO
+
+create table users(
+    id bigserial primary key,
+    created_at timestamp(6),
+    updated_at timestamp(6),
+    email varchar(255) not null unique,
+    name varchar(128) not null,
+    password varchar(255) not null,
+    username varchar(255) not null unique
+);
+
+alter table users
+    owner to chat;
+
---
+++ chat-web-sockets/src/main/resources/db/changelog/changesets/V1691609166_create_table_users_role.sql
@@ -0,0 +1,13 @@
+-- liquibase formatted sql
+
+-- changeset gnori-zon:V1691609166_create_table_users_role:\nGO
+
+create table users_role(
+    user_id bigint not null,
+    role varchar(255)
+);
+
+alter table users_role add constraint FK_users_role_user_id foreign key (user_id) references users(id) on delete cascade;
+alter table users_role add constraint users_role_role_check
+            check ((role)::text = ANY ((ARRAY ['ADMIN'::character varying, 'USER'::character varying])::text[]));
+alter table users_role add constraint uk_users_role_user_id_role unique (user_id, role);
