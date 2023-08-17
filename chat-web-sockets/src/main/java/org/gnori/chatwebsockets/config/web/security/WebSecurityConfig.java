6a6b0c7b9cf2c53915788264f12e370f9464c544
gnori-zon <konasenkos@gmail.com>
2023-08-18 00:47:19 +0300

    #2 #add #resources, config - added custom login page
    changed WebSecurityConfig, added html code for base pages, added thymeleaf in pom for mapping html resouces (!without him infinite redirect loop (302 code) for login page)

--- chat-web-sockets/src/main/java/org/gnori/chatwebsockets/config/web/security/WebSecurityConfig.java
+++ chat-web-sockets/src/main/java/org/gnori/chatwebsockets/config/web/security/WebSecurityConfig.java
@@ -4,14 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     private final UserDetailsService userDetailsService;
 
@@ -19,20 +24,24 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
-            .formLogin(
-                    login -> login
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/chat")
-            )
-            .logout(logout ->
-                    logout.logoutSuccessUrl("/login")
-            )
-                .userDetailsService(userDetailsService)
-            .authorizeHttpRequests(
-                    request -> request
-                            .requestMatchers("/login", "/registrate").permitAll()
-                            .anyRequest().authenticated()
-            );
+                .authorizeHttpRequests(
+                        request -> request
+                                .requestMatchers("/", "/js/**", "/css/**", "/login").anonymous()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(
+                        login -> login
+                                .loginPage("/")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/main")
+                )
+                .logout(logout ->
+                        logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .deleteCookies("SESSION")
+                                .logoutSuccessUrl("/")
+                )
+                .userDetailsService(userDetailsService);
 
         return http.build();
     }
@@ -41,4 +50,11 @@ public class WebSecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
+        registry.addViewController("/main").setViewName("onlyAuth.html");
+    }
+
 }
